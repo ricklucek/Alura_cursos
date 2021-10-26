99553a8468bae743edbd71c163beed6fa29c56cc
ricklucek <henriquelucek.21@gmail.com>
2021-10-26 14:59:44 -0300

    finishing poo module

---
+++ js introduction/ContaCorrente.js
@@ -0,0 +1,44 @@
+import {cliente} from "./Cliente.js";
+export class contaCorrente {
+    static numeroDeContas = 0;
+    agencia;
+    _cliente;
+
+    _saldo = 0;
+
+    set cliente(novoValor){
+        if (novoValor instanceof cliente){
+
+            this._cliente = novoValor;
+        };
+    };
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        contaCorrente.numeroDeContas += 1;
+    }
+
+    sacar(valor){
+        if (this._saldo > valor){
+            this._saldo -= valor
+            return valor
+        }
+    }
+
+    depositar(valor){
+        if (valor > 0){
+            this._saldo += valor
+            return valor
+        }
+    }
+
+    tranferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
