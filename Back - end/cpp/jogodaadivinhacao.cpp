5fc694bc5473b156c46798b02cf92d71e53225a2
ricklucek <henriquelucek.21@gmail.com>
2022-02-04 12:24:30 -0300

    finalizando aula 1

---
+++ Back - end/cpp/jogodaadivinhacao.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    cout << "**********************************" << endl;
+    cout << "* Bem vindo ao jogo da adivinhacao" << endl;
+    cout << "**********************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O numero secreto e " << numero_secreto << endl;
+}
