004857469e041db90d240f3b26e076fd01580d65
ricklucek <henriquelucek.21@gmail.com>
2022-02-04 16:35:40 -0300

    aula 2 completa

--- Back - end/cpp/jogodaadivinhacao.cpp
+++ Back - end/cpp/jogodaadivinhacao.cpp
@@ -2,10 +2,44 @@
 using namespace std;
 
 int main () {
-    cout << "**********************************" << endl;
-    cout << "* Bem vindo ao jogo da adivinhacao" << endl;
-    cout << "**********************************" << endl;
+    cout << "************************************" << endl;
+    cout << "* Bem vindo ao jogo da adivinhacao *" << endl;
+    cout << "************************************" << endl;
 
-    int numero_secreto = 42;
-    cout << "O numero secreto e " << numero_secreto << endl;
+    const int NUMERO_SECRETO = 42;
+    bool nao_acertou = true;
+    int tentativas = 0;
+
+    while(nao_acertou){
+        tentativas++;
+
+        cout << "tentativa numero " << tentativas << endl;
+
+        int chute;
+        cout << "Qual seu chute? " ;
+        cin >> chute;
+
+        cout << "Seu chute e: " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if(acertou){
+        cout << "Parabens! Voce acertou o numero secreto!" << endl;
+        nao_acertou = false;
+         }
+        else if (maior){
+        cout << "Seu chute foi maior que o numero secreto!" << endl;
+        }
+        else {
+        cout << "Seu chute foi menor que o numero secreto!" << endl;
+        }
+
+     
+    }
+
+    cout << "FIM DE JOGO!" << endl;
+
+    cout << "Voce acertou o numero secreto em "<< tentativas << " tentativas" << endl;
+    
 }
